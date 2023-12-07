bc6bb24502e7b9be316e5725a9c5d0b2f5a5ea2e
kartikmahar05 <133872880+kartikmahar05@users.noreply.github.com>
2023-12-07 19:03:15 +0530

    countdown

---
+++ count.js
@@ -0,0 +1,18 @@
+function clock(){
+let endDate="31 december 2023 23:59:59"
+let end=new Date(endDate)
+let now=new Date()
+let inputs=document.getElementsByTagName("input")
+let diff=(end-now)/1000
+console.log(diff)
+inputs[0].value=Math.floor(diff/3600/24);
+inputs[1].value=Math.floor(diff/3600)%24;
+inputs[2].value=Math.floor(diff/60)%60;
+// inputs[3].value=Math.floor(diff%60);4
+
+inputs[3].value=Math.floor(diff%60/2);
+
+}
+setInterval(clock,1000);
+
+
